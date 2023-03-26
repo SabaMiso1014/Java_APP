2d1d642ac2b8e9f8fe61248f05137d1651548306
本城 夕祐也 <yuyahonjo@HonjoYuya-MacBookPro.local>
2023-03-26 10:57:08 +0900

    Javaアプリ開発

---
+++ JSF_PR/faces-master/README.md
@@ -0,0 +1,28 @@
+# Jakarta Faces
+
+This repository contains the code for Jakarta Faces.
+
+[Online JavaDoc](https://javadoc.io/doc/jakarta.faces/jakarta.faces-api)
+
+Building
+--------
+
+Jakarta Faces can be built by executing the following from the project root:
+
+``mvn clean package``
+
+The API jar can then be found in /app/target.
+The SPEC html/pdf can then be found in /spec/target/generated-docs.
+
+
+Making Changes
+--------------
+
+To make changes, fork this repository, make your changes, and submit a pull request.
+
+About Jakarta Faces
+-------------
+
+Jakarta Faces defines an MVC framework for building user interfaces for web applications, 
+including UI components, state management, event handing, input validation, page navigation, and 
+support for internationalization and accessibility.
---
+++ JSF_PR/faces-master/tck/faces23/disableFaceletToXhtmlMapping/src/test/java/ee/jakarta/tck/faces/test/javaee8/disablefacelettoxhtmlmapping/DisableFaceletToXhtmlIT.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023 Contributors to Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package ee.jakarta.tck.faces.test.javaee8.disablefacelettoxhtmlmapping;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import ee.jakarta.tck.faces.test.util.arquillian.ITBase;
+
+@RunWith(Arquillian.class)
+public class DisableFaceletToXhtmlIT extends ITBase {
+    
+    @Test
+    public void doTest() throws Exception {
+        HtmlPage page = getPage("index.xhtml");
+        assertTrue(page.getBody().asXml().indexOf("ViewState") == -1);
+    }
+
+}
